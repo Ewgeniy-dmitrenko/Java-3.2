6ef0b711def0ab280b78f2796668b9a5fd12e55a
Evgeniy Dmitrenko <ewgeniy.dmitrenko@gmail.com>
2021-05-21 01:02:55 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int balance = 1000;
+        int topUpBalance = 2000;
+        int bonus = topUpBalance / 100;
+        int resultBalance;
+        if (topUpBalance > 1000) {
+            resultBalance = balance + topUpBalance + bonus;
+        } else {
+            resultBalance = balance + topUpBalance;
+        }
+        System.out.println("Остаток на счете:" + resultBalance);
+    }
+}
